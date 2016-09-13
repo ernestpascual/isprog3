49da79101effa3feeec65bf66e910bd3d1fffb1d
ernestpascual <eepascual@gmail.com>
2016-09-13 21:06:04 +0800

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class BankAccount
+    {
+        private string accountNumber;
+        private string accountName;
+        private string accountType;
+        private double accountBalance;
+ 
+
+        public void CreateAccount(string accNum, string accName, string accType)
+        {
+
+            if (accType != "SA" && accType != "CA")
+            {
+                this.accountType = "Invalid!";
+
+            } else
+            {
+                this.accountType = accType;
+            }
+
+            this.accountNumber = accNum;
+            this.accountName = accName;
+
+
+        }
+
+
+        public void Deposit(double depositValue)
+        {
+            this.accountBalance += depositValue;
+
+        }
+
+
+        public void Withdraw(double withdrawValue)
+        {
+            this.accountBalance -= withdrawValue;
+
+        }
+
+
+
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Account number: " + this.accountNumber);
+            Console.WriteLine("Account name: " + this.accountName);
+            Console.WriteLine("Account type: " + this.accountType);
+            Console.WriteLine("Account balance: " + this.accountBalance);
+            Console.WriteLine("");
+
+        }
+
+  
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount customer1 = new BankAccount();
+            BankAccount customer2 = new BankAccount();
+            customer1.CreateAccount("1837234", "Ernest Pascual", "aA");
+            customer1.Deposit(10001230.12);
+            customer1.Withdraw(10201);
+
+            customer2.CreateAccount("1837234", "Jacob C", "SA");
+            customer2.Deposit(10001230.12);
+            customer2.Withdraw(10201);
+
+
+            customer1.PrintInfo();
+            customer2.PrintInfo();
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
