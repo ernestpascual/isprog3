4c63c604ee2c3fb08254d28f3b02e455466e5dec
ernestpascual <eepascual@gmail.com>
2016-09-15 21:14:49 +0800

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                /// Inheritance
+            Student regular = new Student();
+            regular.StudentId = "10283";
+            ///regular.PrintStudent();
+            PrintAnyStudent(regular);
+
+
+
+            Scholar acadaemic1 = new Scholar();
+            acadaemic1.StudentId = "9382872";
+            acadaemic1.Allowance = 323.12M;
+            ///acadaemic1.PrintStudent();
+            PrintAnyStudent(acadaemic1);
+
+            Console.Read();
+
+       
+
+
+        }
+        static void PrintAnyStudent(Student x)
+        {
+            x.PrintStudent();
+
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Student
+    {
+        public string StudentId { get; set; }
+
+      
+        /// <summary>
+        ///  if not virutal it will not be overriden
+        /// </summary>
+        public virtual void PrintStudent()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("STUDENT ID: " + this.StudentId);
+        
+        }
+
+        
+    }
+
+    class Scholar : Student
+    {
+        public decimal Allowance { get; set; }
+      
+        /*
+         OVERRIDE
+
+          public  override void PrintStudent()
+        {
+            base.PrintStudent();
+            Console.Write("Allowance: " + this.Allowance);
+        }
+        */
+
+        public new void PrintStudent()
+        {
+            base.PrintStudent();
+            Console.Write("Allowance: " + this.Allowance);
+        }
+
+
+    }
+
+ 
+}
