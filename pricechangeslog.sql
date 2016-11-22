c4b930965b65d41033eee87b98b4a6eb0ea78e94
ernestpascual <eepascual@gmail.com>
2016-11-23 02:00:19 +0800

    Create pricechangeslog.sql

---
+++ pricechangeslog.sql
@@ -0,0 +1,14 @@
+CREATE TRIGGER priceChangeLogUpdate
+ON inventories
+AFTER UPDATE
+AS
+BEGIN
+       SET NOCOUNT ON;
+       DECLARE @product INT
+       DECLARE @prevPrice INT
+	     DECLARE @curPrice INT
+	     SELECT @product = INSERTED.productID FROM INSERTED
+	     SELECT @prevPrice = DELETED.productPrice FROM DELETED
+       SELECT @curPrice = INSERTED.productPrice FROM INSERTED
+	     INSERT INTO priceChanges (pchangetransID,productID, prevPrice, newPrice, dateChanged)  VALUES ((SELECT ISNULL(MAX(pchangetransID)+1,0) FROM dbo.priceChanges WITH(SERIALIZABLE, UPDLOCK)), @product, @prevPrice, @curPrice, CURRENT_TIMESTAMP)
+END
