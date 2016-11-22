e39d00bbaf92b1e8b6b2d20c1b1c28f2146fc56e
ernestpascual <eepascual@gmail.com>
2016-11-23 03:05:56 +0800

    Update dashboardvalues.sql

--- dashboardvalues.sql
+++ dashboardvalues.sql
@@ -8,4 +8,18 @@ inner join sales b ON b.productID =a.productID
 WHERE DATEPART(mm,transDate) = DATEPART(mm, GETDATE()) 
 AND DATEPART(dd,transDate) = DATEPART(dd, GETDATE()) -- sales for the day
 
---stored procedure
+
+
+-- stored procedure
+create procedure dbo.DashboardPrice
+AS
+select sum(a.productPrice * b.transQtyPurchase) as 'Dashboard'
+from inventories a
+inner join sales b ON b.productID =a.productID 
+UNION
+select sum(a.productPrice * b.transQtyPurchase)from inventories a
+inner join sales b ON b.productID =a.productID
+WHERE DATEPART(mm,transDate) = DATEPART(mm, GETDATE()) 
+AND DATEPART(dd,transDate) = DATEPART(dd, GETDATE())
+
+EXEC dbo.DashboardPrice
