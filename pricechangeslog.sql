8ca5e6293ae173bcdfdcdc4c9ba1294449f79c45
ernestpascual <eepascual@gmail.com>
2016-11-23 02:03:59 +0800

    Update pricechangeslog.sql

--- pricechangeslog.sql
+++ pricechangeslog.sql
@@ -1,3 +1,4 @@
+-- create trigger for price change
 CREATE TRIGGER priceChangeLogUpdate
 ON inventories
 AFTER UPDATE
@@ -12,3 +13,13 @@ BEGIN
        SELECT @curPrice = INSERTED.productPrice FROM INSERTED
 	     INSERT INTO priceChanges (pchangetransID,productID, prevPrice, newPrice, dateChanged)  VALUES ((SELECT ISNULL(MAX(pchangetransID)+1,0) FROM dbo.priceChanges WITH(SERIALIZABLE, UPDLOCK)), @product, @prevPrice, @curPrice, CURRENT_TIMESTAMP)
 END
+
+-- update price
+UPDATE inventories
+SET productPrice = '120'
+WHERE productID = '3'
+
+
+-- check changes
+select * from priceChanges 
+select * from inventories
