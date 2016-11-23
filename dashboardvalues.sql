b300c685a604d6ad0f65fbccf081bef064203494
ernestpascual <eepascual@gmail.com>
2016-11-23 17:08:29 +0800

    Update dashboardvalues.sql

--- dashboardvalues.sql
+++ dashboardvalues.sql
@@ -23,3 +23,18 @@ WHERE DATEPART(mm,transDate) = DATEPART(mm, GETDATE())
 AND DATEPART(dd,transDate) = DATEPART(dd, GETDATE())
 
 EXEC dbo.DashboardPrice
+
+
+--- day sales 
+
+
+
+create procedure dbo.DaySalePrice
+AS
+select sum(a.productPrice * b.transQtyPurchase)from inventories a
+inner join sales b ON b.productID =a.productID
+WHERE DATEPART(mm,transDate) = DATEPART(mm, GETDATE()) 
+AND DATEPART(dd,transDate) = DATEPART(dd, GETDATE())
+
+
+EXEC dbo.DaySalePrice
